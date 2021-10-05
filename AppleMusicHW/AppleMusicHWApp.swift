7248bab7b9baaaf73e3333e9563a388cbf980d6b
a.kravcov <a.kravcov@movavi.com>
2021-10-05 16:56:50 +0700

    Initial Commit

---
+++ AppleMusicHW/AppleMusicHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicHWApp.swift
+//  AppleMusicHW
+//
+//  Created by a.kravcov on 05.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
